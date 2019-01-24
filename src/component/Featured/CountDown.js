04726934d1112de038e4934a80f9df6f5ea1743b
raphealolams <raphealolams@gmail.com>
2019-01-24 01:47:19 +0100

    sleep 4d weak

--- src/component/Featured/CountDown.js
+++ src/component/Featured/CountDown.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
 
 class CountDown extends Component {
 
@@ -9,49 +10,51 @@ class CountDown extends Component {
 
     render() {
         return (
-            <div className="countdown_wrapper">
-                <div className="countdown_top">
-                    Event Starts In
+            <Slide left delay={1000}>
+                <div className="countdown_wrapper">
+                    <div className="countdown_top">
+                        Event Starts In
+                    </div>
+
+                    <div className="countdown_bottom">
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                23
+                            </div>
+                            <div className="countdown_tag">
+                                Days
+                            </div>
+                        </div>
+
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                2
+                            </div>
+                            <div className="countdown_tag">
+                                Hours
+                            </div>
+                        </div>
+
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                3
+                            </div>
+                            <div className="countdown_tag">
+                                Minutes
+                            </div>
+                        </div>
+
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                10
+                            </div>
+                            <div className="countdown_tag">
+                                Seconds
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="countdown_bottom">
-                    <div className="countdown_item">
-                        <div className="countdown_time">
-                            23
-                        </div>
-                        <div className="countdown_tag">
-                            Days
-                        </div>
-                    </div>
-
-                    <div className="countdown_item">
-                        <div className="countdown_time">
-                            2
-                        </div>
-                        <div className="countdown_tag">
-                            Hours
-                        </div>
-                    </div>
-
-                    <div className="countdown_item">
-                        <div className="countdown_time">
-                            3
-                        </div>
-                        <div className="countdown_tag">
-                            Minutes
-                        </div>
-                    </div>
-
-                    <div className="countdown_item">
-                        <div className="countdown_time">
-                            10
-                        </div>
-                        <div className="countdown_tag">
-                            Seconds
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </Slide>
         );
     }
 }
